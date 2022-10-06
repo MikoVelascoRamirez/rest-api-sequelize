41171c189131535626fa68c68f0e285d7535eaaf
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-10-06 15:38:35 -0500

    13. feat: router's Project ended

--- routes/projects.js
+++ routes/projects.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getProjects, createProject} = require('../controllers/projects/projects');
+const { getProjects, createProject, updateProject, deleteProject, getProject} = require('../controllers/projects/projects');
 
 router.get('/', getProjects);
 router.post('/', createProject);
-router.put('/:id');
-router.delete('/:id');
-router.get('/:id');
+router.put('/:id', updateProject);
+router.delete('/:id', deleteProject);
+router.get('/:id', getProject);
 
 module.exports = router;
