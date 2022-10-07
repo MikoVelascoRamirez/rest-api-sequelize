85e173da26b7251488c18e5a6cdb284edbaa086c
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-10-06 20:21:42 -0500

    16. feat: adding task router as a middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const projectsRouter = require('./routes/projects')
+const tasksRouter = require('./routes/tasks')
 
 app.use(express.json({ extended: true }));
 app.use('/projects', projectsRouter);
+app.use('/tasks', tasksRouter);
 
 module.exports = app;
