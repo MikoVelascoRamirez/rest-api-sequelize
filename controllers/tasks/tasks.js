b801e167e230adcfa13aaf5c03646a835bec4377
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-10-06 22:24:38 -0500

    19. feat: implementing updateTask controller

--- controllers/tasks/tasks.js
+++ controllers/tasks/tasks.js
@@ -19,11 +19,39 @@ const getTasks = async (req, res) => {
     }
 }
 
+const getTask = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const singleTask = await Task.findByPk(id);
+        if(singleTask === null) throw new Error('Task not found')
+        return res.status(200).send(singleTask);
+    } catch (error) {
+        return res.status(404).json({ error: error.message})
+    }
+}
+
+const updateTask = async (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+
+    try {
+        const task = await Task.findByPk(id);
+
+        if(task === null) throw new Error(`Task can't be updated`)
+        task.set({...body})
+        await task.save();
+
+        res.status(200).send("The task has been updated");
+    } catch (error) {
+        return res.status(404).json({ error: error.message});
+    }
+}
+
 const deleteTask = async (req, res) => {
     const id = req.params.id;
     try {
-        const deleted = await Task.destroy({ where: { id } })
-        if(deleted === 0) throw new Error('La tarea no se encontró para ser eliminado')
+        const deletedTask = await Task.destroy({ where: { id } })
+        if(deletedTask === 0) throw new Error('La tarea no se encontró para ser eliminado')
         res.sendStatus(204);
     } catch (error) {
         res.status(500).json({ error: error.message})
@@ -33,5 +61,7 @@ const deleteTask = async (req, res) => {
 module.exports = {
     createTask,
     getTasks,
+    getTask,
+    updateTask,
     deleteTask
 }
